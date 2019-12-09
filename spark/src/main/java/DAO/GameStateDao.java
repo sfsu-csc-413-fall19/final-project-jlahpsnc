5006275003faf339d507d3a2de010c4d19ef6319
Pedro Souto <Pedro.Souto.SFSU@gmail.com>
2019-12-08 17:21:21 -0800

    Joker half added

--- spark/src/main/java/DAO/GameStateDao.java
+++ spark/src/main/java/DAO/GameStateDao.java
@@ -19,54 +19,56 @@ public class GameStateDao {
 
     public boolean flipCard(int gameId, String playerAttemptingToFlipCard, int x, int y) {
         GameStateDto game = MainServer.getGameById(gameId);
-        if (game != null && game.currentPlayersTurn.equals(playerAttemptingToFlipCard)) {
+        if (game != null && game.currentPlayersTurn.equals(playerAttemptingToFlipCard) && game.gameBoard.getCard(x,y) != null) {
             // If no other card has been flipped before, simply flip this card over
             if (game.cardFlipped == null) {
-                if (game.gameBoard.getCard(x,y) != null) {
-                    // If the card is a joker
-                    if (game.gameBoard.getCard(x,y).cardId == 99) {
-                        updatePlayerScore(gameId, game.currentPlayersTurn);
-                        game.gameBoard.getCard(x,y).isPaired = true;
-                        game.gameBoard.getCard(x,y).isRevealed = true;
-                    } else {
-                        game.gameBoard.getCard(x, y).isRevealed = true;
-                        game.cardFlipped = game.gameBoard.getCard(x, y);
-                    }
-                    WebSocketHandler.updateGame(game);
+                // If the card is a joker
+                if (game.gameBoard.getCard(x,y).cardId == 99) {
+                    updatePlayerScore(gameId, game.currentPlayersTurn);
+                    game.gameBoard.getCard(x,y).isPaired = true;
+                    game.gameBoard.getCard(x,y).isRevealed = true;
+                    game.jokerIsRevealed = true;
+                } else {
+                    game.gameBoard.getCard(x, y).isRevealed = true;
+                    game.cardFlipped = game.gameBoard.getCard(x, y);
                 }
+                WebSocketHandler.updateGame(game);
             }
             // If a card has been flipped before, then we need to check if this new card bring flipped matches it
             else {
-                if (game.gameBoard.getCard(x,y) != null) {
-                    // If the new card being flipped matches the already flipped card
-                    if (game.gameBoard.getCard(x,y).cardId == game.cardFlipped.cardId) {
-                        game.gameBoard.getCard(game.cardFlipped.x, game.cardFlipped.y).isPaired = true;
-                        game.gameBoard.getCard(game.cardFlipped.x, game.cardFlipped.y).isRevealed = true;
-                        game.cardFlipped = null;
-                        game.gameBoard.getCard(x,y).isPaired = true;
-                        game.gameBoard.getCard(x,y).isRevealed = true;
+                // If the new card being flipped matches the already flipped card
+                if (game.gameBoard.getCard(x,y).cardId == game.cardFlipped.cardId) {
+                    game.gameBoard.getCard(game.cardFlipped.x, game.cardFlipped.y).isPaired = true;
+                    game.gameBoard.getCard(game.cardFlipped.x, game.cardFlipped.y).isRevealed = true;
+                    game.cardFlipped = null;
+                    game.gameBoard.getCard(x, y).isPaired = true;
+                    game.gameBoard.getCard(x, y).isRevealed = true;
 
-                        updatePlayerScore(gameId, game.currentPlayersTurn);
-                        game.numPairsLeft--;
+                    updatePlayerScore(gameId, game.currentPlayersTurn);
+                    game.numPairsLeft--;
 
-                        if (checkForGameOver(gameId)) {
-                            game.gameIsOver = true;
-                            WebSocketHandler.gameOverBroadcast(game);
-                        } else {
-                            WebSocketHandler.updateGame(game);
-                        }
-                    }
-                    // If the new card being flipped does not match the already flipped card
-                    else {
-                        game.gameBoard.getCard(game.cardFlipped.x, game.cardFlipped.y).isPaired = false;
-                        game.gameBoard.getCard(game.cardFlipped.x, game.cardFlipped.y).isRevealed = false;
-                        game.cardFlipped = null;
-                        game.gameBoard.getCard(x,y).isPaired = false;
-                        game.gameBoard.getCard(x,y).isRevealed = false;
-
-                        changeTurns(gameId);
+                    if (checkForGameOver(gameId)) {
+                        game.gameIsOver = true;
+                        WebSocketHandler.gameOverBroadcast(game);
+                    } else {
                         WebSocketHandler.updateGame(game);
                     }
+                }
+                else if (game.gameBoard.getCard(x,y).cardId == 99){
+                    /* TODO
+                    When jokes is the second card, it behaves differently
+                     */
+                }
+                // If the new card being flipped does not match the already flipped card
+                else {
+                    game.gameBoard.getCard(game.cardFlipped.x, game.cardFlipped.y).isPaired = false;
+                    game.gameBoard.getCard(game.cardFlipped.x, game.cardFlipped.y).isRevealed = false;
+                    game.cardFlipped = null;
+                    game.gameBoard.getCard(x,y).isPaired = false;
+                    game.gameBoard.getCard(x,y).isRevealed = false;
+
+                    changeTurns(gameId);
+                    WebSocketHandler.updateGame(game);
                 }
             }
             return true;
