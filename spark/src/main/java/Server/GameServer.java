d49f4ca160fc7c46dac0941b28bc69fa55edda9a
Pedro Souto <Pedro.Souto.SFSU@gmail.com>
2019-12-12 18:54:09 -0800

    Added static file location

--- spark/src/main/java/Server/GameServer.java
+++ spark/src/main/java/Server/GameServer.java
@@ -29,6 +29,7 @@ public class GameServer {
 
     public static void main(String[] args) {
     port(1234);
+    staticFiles.externalLocation(System.getProperty("user.dir") + "/react"); // Static files location
 
     webSocket("/wsLoading", WebSocketHandler.class);
 
